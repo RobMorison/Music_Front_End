7d9b850927abe4ea4f8f1f06a54feee4ae5cf86a
Sara Morison <rob.morison32@gmail.com>
2023-03-01 14:27:33 -0500

    Created Music table component and table

---
+++ src/Components/MusicTable/MusicTable.jsx
@@ -0,0 +1,30 @@
+const MusicTable = (props) => {
+    return ( 
+        <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Artist</th>
+            <th>Album</th>
+            <th>Release Date</th>
+            <th>Genre</th>
+          </tr>
+        </thead>
+        <tbody>
+          {songs.map((songs, index) => {
+            return (
+              <tr key={index}>
+                <td>{songs.title}</td>
+                <td>{songs.artist}</td>
+                <td>{songs.album}</td>
+                <td>{songs.release_date}</td>
+                <td>{songs.genre}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+     );
+}
+ 
+export default MusicTable;
